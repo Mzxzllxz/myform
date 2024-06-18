1665f33ba338316268b16b5e1f1e38b6c6429572
Mzxzllxz <xzllxz456@gmail.com>
2024-06-18 18:09:07 +0900

    querydsl setting

---
+++ src/main/java/com/springboot/test/myform/config/datasource/jpa/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package com.springboot.test.myform.config.datasource.jpa;
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
